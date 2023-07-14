1298d89cd4202ab530d6b90bdd7eabb60ab3230b
mitraillet <steve.henriquet@gmail.com>
2023-07-14 10:46:20 +0200

    first succes with grid

--- js/index.js
+++ js/index.js
@@ -42,3 +42,89 @@ age.appendChild(ageValue)
 //     });
 //   }
 // }
+
+
+let postsData = "";
+let filterData = "";
+const postsContainer = document.querySelector(".posts-container");
+const filterContainer = document.querySelector(".filter-container");
+
+fetch('https://raw.githubusercontent.com/mitraillet/mitraillet.github.io/Pure-HTML-CSS/data/portfolio.json'
+).then(async (response) => {
+  postsData = await response.json();
+  postsData.map((post) => createPost(post));
+  filterData = [
+    ...new Set(
+      postsData
+        .map((post) => post.categories)
+        .reduce((acc, curVal) => acc.concat(curVal), [])
+    )
+  ];
+  filterData.map((filter) => createFilter(filter));
+});
+
+const createPost = (postData) => {
+  const { title, description, image, categories } = postData;
+  const post = document.createElement("div");
+  post.className = "post";
+  post.innerHTML = `
+      <div class="post-preview view effect">
+        <img class="post-image" src="${image}">
+        <div class="mask">
+        <h4>${title}</h4>
+        <a href="#" class="info" title="Full Image">"${description}"</a></div>
+      </div>
+  `;
+
+  postsContainer.append(post);
+};
+
+const createFilter = (filter) => {
+  const filterButton = document.createElement("button");
+  filterButton.className = "filter-button btn w3-text-blue-grey w3-margin w3-large";
+  filterButton.innerText = filter;
+  filterButton.setAttribute('data-state', 'inactive');
+  filterButton.addEventListener("click", (e) =>
+    handleButtonClick(e, filter)
+  );
+
+  filterContainer.append(filterButton);
+};
+
+const resetFilterButtons = (currentButton) => {
+  const filterButtons = document.querySelectorAll('.filter-button');
+  [...filterButtons].map(button => {
+    if (button != currentButton) {
+      button.classList.remove('is-active');
+      button.setAttribute('data-state', 'inactive')
+    }
+  })
+}
+
+const handleButtonClick = (e, param) => {
+  const button = e.target;
+  const buttonState = button.getAttribute('data-state');
+  resetFilterButtons(button);
+  
+  if (buttonState =='inactive') {
+    button.classList.add('is-active');
+    button.setAttribute('data-state', 'active');
+    handleFilterPosts(param)
+  } else {
+    button.classList.remove('is-active');
+    button.setAttribute('data-state', 'inactive')
+    resetPosts()
+  }
+}
+
+const handleFilterPosts = (param) => {
+  let filteredPosts = [...postsData].filter(post => post.categories.includes(param))
+  
+  postsContainer.innerHTML = "";
+  filteredPosts.map(post => createPost(post))
+};
+
+const resetPosts = () => {
+  postsContainer.innerHTML = "";
+  postsData.map((post) => createPost(post));
+}
